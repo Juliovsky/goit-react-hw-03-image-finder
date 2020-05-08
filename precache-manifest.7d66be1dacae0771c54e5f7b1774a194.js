6d9b26f1edc427577e48792753d6bf57c29ff030
Yuliia <ybelova24@gmail.com>
2020-05-08 20:38:43 +0300

    Updates

---
+++ precache-manifest.7d66be1dacae0771c54e5f7b1774a194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77fa98047f22e250421e22ade00fd3e",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "8f0d4149732f20859bb0",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.69dbaf2d.chunk.css"
+  },
+  {
+    "revision": "ca7ca93be4fd91e28efe",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c87973e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c87973e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0d4149732f20859bb0",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c4aee4ca.chunk.js"
+  },
+  {
+    "revision": "2c33b5407f6ddb0305a4",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.395ab540.js"
+  }
+]);
